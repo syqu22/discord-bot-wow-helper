59a4c17099df8c89859f35fd2f0e3662af4781cd
syqu22 <aleklejawa@gmail.com>
2022-01-23 23:55:40 +0100

    Slash commands implemented

---
+++ deploy-commands.js
@@ -0,0 +1,38 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const { token } = require("./config.json");
+
+// For testing
+const clientId = "934929013238034442";
+const guildId = "842688263445282817";
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName("log")
+    .setDescription(
+      "Shows fights from WarcraftLogs log as a list with clickable link to each fight."
+    ),
+  new SlashCommandBuilder()
+    .setName("affixes")
+    .setDescription(
+      "Shows affixes for previous/current/next week. Can also show affixes for the week user wants."
+    ),
+  new SlashCommandBuilder()
+    .setName("token")
+    .setDescription(
+      "Shows current price of the WoW token, data is taken from EU, US, KR, TW and CN regions."
+    ),
+  new SlashCommandBuilder()
+    .setName("character")
+    .setDescription(
+      "Shows information about given character. Make sure character name and realm."
+    ),
+].map((command) => command.toJSON());
+
+const rest = new REST({ version: "9" }).setToken(token);
+
+rest
+  .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+  .then(() => console.log("Commands successfully updated."))
+  .catch(console.error);
--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const { Client, Intents } = require("discord.js");
-const config = require("./config.json");
+const { token } = require("./config.json");
 
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS],
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
 });
 
 client.once("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.login(config.token);
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) {
+    return;
+  }
+
+  const { commandName } = interaction;
+
+  if (commandName === "log") {
+    await interaction.reply("log");
+  } else if (commandName === "affixes") {
+    await interaction.reply("affixes");
+  } else if (commandName === "token") {
+    await interaction.reply("token");
+  } else if (commandName === "character") {
+    await interaction.reply("character");
+  }
+});
+
+client.login(token);
