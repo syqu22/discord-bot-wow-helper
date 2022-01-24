e84a1b1c1f3b9cb343502af9912dc1bd4eb55ae7
syqu22 <aleklejawa@gmail.com>
2022-01-24 16:43:53 +0100

    Small tweak to affixes

--- affixes.js
+++ affixes.js
@@ -1,4 +1,4 @@
-const data = require("./affixes.json");
+import data from "./data/affixes-data.json";
 
 const weeksSinceFirstWeek = () => {
   const timeDifference = new Date() - new Date(data["first_week"]);
@@ -6,54 +6,45 @@ const weeksSinceFirstWeek = () => {
   return Math.floor(timeDifference / (1000 * 3600 * 24 * 7) + 1);
 };
 
-const currentAffixes = () => {
+export const currentAffixes = () => {
   let week = weeksSinceFirstWeek();
   const currentWeek = week;
 
-  while (week > 12) {
-    week -= 12;
+  if (week > 12) {
+    week %= 12;
   }
   return { week: currentWeek, affixes: data.affixes[week - 1] };
 };
 
-const nextAffixes = () => {
+export const nextAffixes = () => {
   let week = weeksSinceFirstWeek() + 1;
   const nextWeek = week;
 
-  while (week > 12) {
-    week -= 12;
+  if (week > 12) {
+    week %= 12;
   }
   return { week: nextWeek, affixes: data.affixes[week - 1] };
 };
 
-const previousAffixes = () => {
+export const previousAffixes = () => {
   let week = weeksSinceFirstWeek() - 1;
   const previousWeek = week;
 
   if (week < 1) {
     return { error: "No affixes" };
-  }
-
-  while (week > 12) {
-    week -= 12;
+  } else if (week > 12) {
+    week %= 12;
   }
   return { week: previousWeek, affixes: data.affixes[week - 1] };
 };
 
-const affixesFromWeek = (week) => {
+export const affixesFromWeek = (week) => {
   const givenWeek = week;
 
   if (week < 1) {
     return { error: "No affixes" };
-  }
-
-  while (week > 12) {
-    week -= 12;
+  } else if (week > 12) {
+    week %= 12;
   }
   return { week: givenWeek, affixes: data.affixes[week - 1] };
 };
-
-exports.currentAffixes = currentAffixes;
-exports.nextAffixes = nextAffixes;
-exports.previousAffixes = previousAffixes;
-exports.affixesFromWeek = affixesFromWeek;
