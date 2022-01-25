b0e64957fe672d6896cf05da5304d8036a6f120a
syqu22 <aleklejawa@gmail.com>
2022-01-25 03:30:34 +0100

    Working Blizzard API connetion added

--- wow.js
+++ wow.js
@@ -4,23 +4,38 @@ import config from "./config.json";
 const BLIZZARD_CLIENT = config.blizzard_client;
 const BLIZZARD_SECRET = config.blizzard_secret;
 
-const retrieveToken = async () => {
-  return await axios
-    .post(
-      "https://us.battle.net/oauth/token",
-      {},
-      {
-        params: {
-          grant_type: "client_credentials",
-          client_id: BLIZZARD_CLIENT,
-          client_secret: BLIZZARD_SECRET,
-        },
-      }
-    )
-    .then((res) => {
-      return res.data;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
+const getAPIConnection = async () => {
+  try {
+    await axios
+      .post(
+        "https://us.battle.net/oauth/token",
+        {},
+        {
+          params: {
+            grant_type: "client_credentials",
+            client_id: BLIZZARD_CLIENT,
+            client_secret: BLIZZARD_SECRET,
+          },
+        }
+      )
+      .then(async (res) => {
+        await axios
+          .get(
+            "https://us.api.blizzard.com/data/wow/token/index?namespace=dynamic-us",
+
+            {
+              headers: {
+                Authorization: `Bearer ${res.data.access_token}`,
+              },
+            }
+          )
+          .then((res) => {
+            console.log(res);
+          });
+      });
+  } catch (err) {
+    console.log(err.message);
+  }
 };
+
+getAPIConnection();
